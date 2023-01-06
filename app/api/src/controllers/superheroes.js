3fc054c6f55e7c7925d1b5e003813af52bb8ae91
Eduardo <72766650+odraudep@users.noreply.github.com>
2023-01-05 23:42:48 -0300

    add get super heroes route

--- app/api/src/controllers/superheroes.js
+++ app/api/src/controllers/superheroes.js
@@ -1,3 +1,31 @@
-export const getSuperheroes = (req, res) => {
-  res.json({ message: 'heroes' });
+import axios from 'axios';
+
+export const getSuperheroes = async (req, res) => {
+  const offset = Number(req.query.offset) || 1;
+  const limit = Number(req.query.limit) || 1;
+  let heroes = [];
+
+  if (offset < 1 || limit < 1) {
+    return res
+      .status(400)
+      .json({
+        message: 'Provide a offset or limit value greater than 1',
+      });
+  }
+
+  try {
+    for (let count = offset; count < (offset + limit); count++) {
+      const { data } = await axios.get(`https://superheroapi.com/api/203294025607288/${count}`);
+
+      heroes = [...heroes, data];
+    }
+
+    res.json({ results: heroes });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        message: 'There was an error in the server. Try again later',
+      });
+  }
 };
